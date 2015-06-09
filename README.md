964c7bcd4b1316f45abc613e728cd35b054f5905
RangerMauve <RangerMauve@htomail.com>
2015-06-08 23:50:34 -0400

    Progress on implementation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mqtt-store
 Store and retrieve data using MQTT keypaths
+
+**NOT READY YET**
---
+++ index.js
@@ -0,0 +1,101 @@
+module.exports = MQTTStore;
+
+function MQTTStore() {
+	if (!(this instanceof MQTTStore))
+		return new MQTTStore();
+	this.tree = {
+		children: {}
+	};
+}
+
+MQTTStore.prototype = {
+	tree: null,
+	get: get,
+	query: query,
+	set: set
+};
+
+function get(key) {
+	return get_one(split(key), this.tree);
+}
+
+function query(key) {
+	return get_all(split(key), this.tree);
+}
+
+function set(key, value) {
+	set_one(split(key), this.tree, value);
+	return value;
+}
+
+function remove_one(path, tree) {
+	if (!path.length) return;
+	var next = path[0];
+	if (!(next in tree.children)) return;
+	return remove_one(path.slice(1), tree);
+}
+
+function set_one(path, tree, value) {
+	if (!path.length) return tree.value = value;
+	var next = path[0];
+	var children = tree.children;
+	var next_tree = children[next];
+	if (!next_tree)
+		next_tree = children[next] = {
+			children: {}
+		};
+	set_one(path.slice(1), tree.children[next], value);
+}
+
+function get_one(path, tree) {
+	if (!path.length) return tree.value;
+	var next = path[0];
+	var next_tree = tree.children[next];
+	if (!next_tree) return undefined;
+	return get_one(path.slice(1), next_tree);
+}
+
+function get_all(path, tree) {
+	if (!path.length)
+		return [tree.value];
+
+	var next = path[0];
+
+	if (next === "+") return values(tree.children)
+		.map(get_all.bind(null, path.slice(1)))
+		.reduce(flatten, []);
+
+	if (next === "#") return all_values(tree);
+
+	var next_tree = tree.children[next];
+	if (!next_tree) return [];
+	return get_all(path.slice(1), next_tree);
+}
+
+function all_values(tree) {
+	return values(tree.children).map(function(subtree) {
+		var all = all_values(subtree);
+		var current = subtree.value;
+		if (current !== undefined)
+			all.push(current);
+		return all;
+	}).reduce(flatten, []);
+}
+
+function keys(object) {
+	return Object.keys(object);
+}
+
+function values(object) {
+	return keys(object).map(function(key) {
+		return object[key];
+	});
+}
+
+function flatten(prev, current) {
+	return prev.concat(current);
+}
+
+function split(path) {
+	return path.split("/");
+}
