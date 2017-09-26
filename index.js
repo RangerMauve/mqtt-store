49b335417dcb0554ef17f2a129384b8dbee10513
RangerMauve <RangerMauve@htomail.com>
2017-09-25 22:09:36 -0400

    Initial progress on code rewrite

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 "use strict";
 module.exports = MQTTStore;
 
+// CONSTANTS
+var NO_RESULT = new Result([], null);
+var WILDCARD_ALL = "#";
+var WILDCARD_ONE = "+";
+
+MQTTStore.NO_RESULT = NO_RESULT;
+MQTTStore.Tree = Tree;
+MQTTStore.Result = Result;
+
+// TYPE DECLARATIONS
+
 function MQTTStore() {
 	if (!(this instanceof MQTTStore))
 		return new MQTTStore();
@@ -11,141 +22,182 @@ function MQTTStore() {
 
 MQTTStore.prototype = {
 	tree: null,
+
 	get: get,
-	query: query,
-	match: match,
-	set: set
+	put: put,
+	findMatching: findMatching,
+	findPatterns: findPatterns,
 };
 
+function Result(sections, value) {
+	this.sections = sections;
+	this.value = value;
+	this.key = sections.join("/");
+}
+
+Result.prototype = {
+	key: "",
+	sections: null,
+	value: null,
+};
 
 function Tree() {
-	this.children = [];
+	this.children = {};
 }
 
 Tree.prototype = {
-	children: [],
-	value: undefined
+	hasValue: false,
+	children: null,
+	value: null,
 };
 
+// METHOD DEFINITIONS
+
+function put(key, value) {
+	var sections = key.split("/");
+	if (!sections.length) return this;
+	var tree = this.tree;
+	put_value(sections, tree, value, 0);
+	return this;
+}
+
 function get(key) {
-	return get_one(split(key), this.tree);
+	var sections = key.split("/");
+	if (!sections.length) return NO_RESULT;
+	var tree = this.tree;
+	return get_value(sections, tree, 0);
 }
 
-function query(key) {
-	return get_all(split(key), this.tree);
+function findMatching(pattern) {
+	var sections = pattern.split("/");
+	checkValid(sections);
+	var results = [];
+	if (!sections.length) return results;
+	var tree = this.tree;
+	add_matching(sections, tree, [], results, 0);
 }
 
-function match(key) {
-	return get_matching(split(key), this.tree);
+function findPatterns(key) {
+	checkBasic(key);
 }
 
-function set(key, value) {
-	set_one(split(key), this.tree, value);
-	return value;
+// ABSTRACT OPERATIONS
+
+function getChild(tree, section) {
+	var child = tree.children[section];
+	if (!child) return null;
+	return child;
 }
 
-function set_one(path, tree, value) {
-	if (!path.length) {
-		tree.value = value;
-		return;
+function put_value(sections, tree, value, index) {
+	var section = sections[index];
+
+	ensureExists(tree, section);
+	var existing = getChild(tree, section);
+
+	if (isLast(sections, index)) {
+		existing.hasValue = true;
+		existing.value = value;
+	} else {
+		put_value(sections, existing, value, index + 1);
 	}
-	var next = path[0];
+}
+
+function ensureExists(tree, section) {
+	if (!tree.children[section])
+		tree.children[section] = new Tree();
+}
+
+function get_value(sections, tree, index) {
+	var section = sections[index];
+	var existing = getChild(tree, section);
+	if (!existing) return NO_RESULT;
+	if (isLast(sections, index))
+		return new Result(sections, existing.value);
+	return get_value(sections, existing, index + 1);
+}
+
+function isLast(sections, index) {
+	return index === (sections.length - 1);
+}
+
+function add_matching(sections, tree, resultSections, results, index) {
+	var section = sections[index];
+	if (!section) {
+		add_value(resultSections, tree, results);
+	} else if (section[0] === WILDCARD_ALL) {
+		addAllWildcard(sections, tree, resultSections, results);
+	} else if (section[0] === WILDCARD_ONE) {
+		addOneWildcard(sections, tree, resultSections, results, index);
+	} else {
+		var nextChild = getChild(tree, section);
+		if (nextChild) {
+			add_matching(sections, nextChild, resultSections.concat(section), results, index + 1);
+		}
+	}
+}
+
+function addAllWildcard(sections, tree, resultSections, results) {
+	if (resultSections.length)
+		add_value(resultSections, tree, results);
+	add_all_values(resultSections, tree, results);
+}
+
+function addOneWildcard(sections, tree, resultSections, results, index) {
+	var names = childNames(tree);
 	var children = tree.children;
-	var next_tree = children[next];
-	if (!next_tree) {
-		next_tree = new Tree();
-		children[next] = next_tree;
+	var length = names.length;
+	var nameIndex = length;
+	for (; nameIndex; nameIndex--) {
+		var currentKey = names[nameIndex];
+		var currentTree = children[currentKey];
+		var currentSections = resultSections.concat(currentKey);
+		add_matching(sections, currentTree, currentSections, results, index + 1);
 	}
-	set_one(path.slice(1), tree.children[next], value);
 }
 
-function get_one(path, tree) {
-	if (!path.length) return tree.value;
-	var next = path[0];
-	var next_tree = tree.children[next];
-	if (!next_tree) return undefined;
-	return get_one(path.slice(1), next_tree);
-}
-
-function get_all(path, tree) {
-	if (!path.length) {
+function add_value(resultSections, tree, results) {
+	if (tree.hasValue) {
 		var value = tree.value;
-		if (value !== undefined) return [value];
-		return [];
+		results.push(new Result(resultSections, value));
 	}
-
-	var next = path[0];
-
-	if (next === "+") return values(tree.children)
-		.map(get_all.bind(null, path.slice(1)))
-		.reduce(flatten, []);
-
-	if (next === "#") return all_values(tree);
-
-	var next_tree = tree.children[next];
-	if (!next_tree) return [];
-	return get_all(path.slice(1), next_tree);
 }
 
-function get_tree_values(tree) {
-	var results = [];
-	var value = tree.value;
-	if (value !== undefined) results.push(value);
-	var multi = tree.children["#"];
-	if (multi && multi.value)
-		results.push(multi.value);
-	return results;
-}
-
-function get_matching(path, tree) {
-	if (!path.length) {
-		return get_tree_values(tree);
+function add_all_values(resultSections, tree, results) {
+	var names = childNames(tree);
+	var children = tree.children;
+	var length = names.length;
+	var nameIndex = length;
+	for (; nameIndex; nameIndex--) {
+		var currentKey = names[nameIndex];
+		var currentTree = children[currentKey];
+		var currentSections = resultSections.concat(currentKey);
+		add_value(currentSections, currentTree, results);
+		add_all_values(currentSections, currentTree, results);
 	}
-
-	var multi = tree.children["#"];
-	var single = tree.children["+"];
-
-	var next = path[0];
-	var next_tree = tree.children[next];
-
-	var rest = path.slice(1);
-
-	var results = [];
-
-	if (multi && multi.value)
-		results.push(multi.value);
-
-	return results.concat([single, next_tree]
-		.reduce(function (all, subtree) {
-			if (subtree) return all.concat(get_matching(rest, subtree));
-			return all;
-		}, []));
 }
 
-function all_values(tree) {
-	return values(tree.children).map(function (subtree) {
-		var all = all_values(subtree);
-		var current = subtree.value;
-		if (current !== undefined)
-			all.push(current);
-		return all;
-	}).reduce(flatten, []);
+function childNames(tree) {
+	var children = tree.children;
+	return Object.keys(children);
 }
 
-function keys(object) {
-	return Object.keys(object);
+// Validation functions
+
+function checkValid(sections) {
+	var length = sections.length;
+	for (var index = 0; index < length; index++) {
+		var current = sections[index];
+		if (current[0] === WILDCARD_ALL) {
+			if (!isLast(sections, index))
+				throw new TypeError("# wildcard can only be at the end of a pattern. In: " + sections.join("/"));
+		}
+	}
 }
 
-function values(object) {
-	return keys(object).map(function (key) {
-		return object[key];
-	});
-}
-
-function flatten(prev, current) {
-	return prev.concat(current);
-}
-
-function split(path) {
-	return path.split("/");
+function checkBasic(key) {
+	var hasAll = key.indexOf(WILDCARD_ALL) !== -1;
+	var hasOne = key.indexOf(WILDCARD_ONE) !== -1;
+	if (hasAll || hasOne)
+		throw new TypeError("Cannot use key with wildcards finding patterns: " + key);
 }
--- test/index.js
+++ test/index.js
@@ -2,168 +2,22 @@
 var test = require("tape");
 
 var MQTTStore = require("../");
+var Result = MQTTStore.Result;
 
-test("MQTTStore#set(key, value): should be able to set a topic with one level", function (t) {
+test("Basic operations", function (t) {
+	t.plan(4);
 	var store = new MQTTStore();
-	t.plan(1);
-	store.set("1", "1");
-	t.equal(store.tree.children[1].value, "1", "key is set to \"1\"");
-});
+	store.put("foo/bar", "baz");
+	t.pass("Able to put simple path into store");
 
-test("MQTTStore#set(key, value): should be able to set a nested topic", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("2/2", "2");
-	t.equal(store.tree.children[2].children[2].value, "2", "key is set to \"2\"");
-});
+	store.put("foo/+/#", "fizz");
+	t.pass("Able to put path with wildcards into store");
 
-test("MQTTStore#set(key, value): should be able to overwrite an existing topic", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("3", "3");
-	store.set("3", "33");
-	t.equal(store.tree.children[3].value, "33", "key is set to \"33\"");
-});
+	var result1 = store.get("foo/bar");
+	t.deepEqual(result1,  new Result(["foo", "bar"], "baz"));
 
-test("MQTTStore#set(key, value): should accept wildcards as though they were regular part of the path", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("4/+/4/#", "4");
-	t.equal(store.tree.children[4].children["+"].children["4"].children["#"].value, "4", "key is set to \"4\"");
-});
+	var result2 = store.get("foo/+/#");
+	t.deepEqual(result2,  new Result(["foo", "+", "#"], "fizz"));
 
-test("MQTTStore#get(key): should return undefined on keys that don't exist", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	var value = store.get("1");
-	t.equal(value, undefined, "value is undefined");
-});
-
-test("MQTTStore#get(key): should return the value set at a non-nested key path", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("2", 2);
-	var value = store.get("2");
-	t.equal(value, 2, "value is 2");
-});
-
-test("MQTTStore#get(key): should return deeply nested values properly", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("3/3/3/3", 3);
-	var value = store.get("3/3/3/3");
-	t.equal(value, 3, "value is three");
-});
-
-test("MQTTStore#get(key): should handle wildcards like reguar keys", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("4/+/4/#", 4);
-	var value = store.get("4/+/4/#");
-	t.equal(value, 4, "value is 4");
-});
-
-test("MQTTStore#query(topic): should return an empty array when there are no matches", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	var values = store.query("1/+");
-	t.deepEqual(values, [], "is an empty array");
-});
-
-test("MQTTStore#query(topic): should return an array of values that match single level wildcards", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("2/2", "2");
-	store.set("22", "2");
-	store.set("2/22", "2");
-	var values = store.query("2/+");
-	t.deepEqual(values, ["2", "2"], "is array with two elements of \"2\"");
-});
-
-test("MQTTStore#query(topic): should get all values in sub-paths with multi level wildcards", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("3/3/3", "3");
-	store.set("3/3", "3");
-	store.set("3/3/3/3", "3");
-	store.set("3", "3");
-	var values = store.query("#");
-	t.deepEqual(values, ["3", "3", "3", "3"], "is array with 4 3s");
-});
-
-test("MQTTStore#query(topic): should support a mix of both wildcards", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("4/4/4", "4");
-	store.set("4/44/4/4", "4");
-	store.set("4/444/444", "4");
-	var values = store.query("4/+/#");
-	t.deepEqual(values, ["4", "4", "4"], "is array with 4 4s");
-});
-
-test("MQTTStore#query(topic): shouldn't return undefined matches (ones that got unset)", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("5/5/5", "5");
-	store.set("5/5/5", undefined);
-	var values = store.query("5/#");
-	t.deepEqual(values, [], "is an empty array");
-});
-
-test("MQTTStore#match(topic): should return an empty array when there are no matches", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	var values = store.query("1/1/1");
-	t.deepEqual(values, [], "is an empty array");
-});
-
-test("MQTTStore#match(topic): should return direct matches only once", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("2/2", "2");
-	var values = store.match("2/2");
-	t.deepEqual(values, ["2"], "is an array with one 2");
-});
-
-test("MQTTStore#match(topic): should return matches on single wildcards", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("3/+", "3");
-	var values = store.match("3/3");
-	t.deepEqual(values, ["3"], "is an array with one 3");
-});
-
-test("MQTTStore#match(topic): should return matches on multi wildcards", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("4/#", "4");
-	var values = store.match("4/4");
-	t.deepEqual(values, ["4"], "is an array with one 4");
-});
-
-test("MQTTStore#match(topic): should return matches on multi wildcards that match the end of the topic", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("5/5/#", "5");
-	var values = store.match("5/5");
-	t.deepEqual(values, ["5"], "is an array with one 5");
-});
-
-test("MQTTStore#match(topic): should find all wildcards that match the topic", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("6/+/6", "6");
-	store.set("6/6/6", "6");
-	store.set("6/#", "6");
-	var values = store.match("6/6/6");
-	t.deepEqual(values, ["6", "6", "6"], "is array with 3 6s");
-});
-
-test("MQTTStore#match(topic): shouldn't return undefiend matches (ones that got unset)", function (t) {
-	t.plan(1);
-	var store = new MQTTStore();
-	store.set("7/7/7", "7");
-	store.set("7/7/7", undefined);
-	var values = store.match("7/7/7");
-	t.deepEqual(values, [], "it is an empty array");
+	t.end();
 });
